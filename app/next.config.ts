f2adcf741543cb98c7c1a66c8ba7e1402fb6e1fa
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-10-31 18:20:57 +0900

    AWSインフラを追加

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@mastra/*"],
+  output: "standalone",
 };
 
 export default nextConfig;
